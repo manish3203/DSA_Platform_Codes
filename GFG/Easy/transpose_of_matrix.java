7bcab260c641048244f10f451fb8a857c195c250
manish3203 <manishchavan3203@gmail.com>
2024-01-29 19:04:44 +0530

    Transpose Of Matrix

---
+++ GFG/Easy/transpose_of_matrix.java
@@ -0,0 +1,58 @@
+/*
+
+	Transpose of Matrix
+
+	Company: MakeMyTrip, InfoEdge, Bloomberg
+	Platform: GFG
+	Write a program to find the transpose of a square matrix of size N*N. Transpose of a matrix is
+	obtained by changing rows to columns and columns to rows.
+
+	Example 1:
+	Input:
+	N = 4
+	mat[][] = 
+	{{1, 1, 1, 1},
+	{2, 2, 2, 2}
+	{3, 3, 3, 3}
+	{4, 4, 4, 4}}
+	Output:
+	{{1, 2, 3, 4},
+	{1, 2, 3, 4}
+	{1, 2, 3, 4}
+	{1, 2, 3, 4}}
+
+	Example 2:
+	Input:
+	N = 2
+	mat[][] = {{1, 2},
+	{-9, -2}}
+	Output:
+	{{1, -9},
+	{2, -2}}
+
+	Expected Time Complexity: O(N * N)
+	Expected Auxiliary Space: O(1)
+
+	Constraints:
+	1 <= N <= 103
+	-109 <= mat[i][j] <= 109
+
+*/
+
+
+class Solution
+{
+    public void transpose(int n,int a[][])
+    {
+
+        for(int i=0; i<n; i++) {
+
+            for(int j=0; j<=i; j++) {
+
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+            }
+        }
+    }
+}
