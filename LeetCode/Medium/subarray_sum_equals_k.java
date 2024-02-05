1bcc576fddcc08bcb37c863fd8ac221b4e88f26c
manish3203 <manishchavan3203@gmail.com>
2024-02-05 23:43:13 +0530

    560_Subarray Sum Equals K

---
+++ LeetCode/Medium/subarray_sum_equals_k.java
@@ -0,0 +1,54 @@
+/*
+
+	Subarray Sum Equals K (Subarray with given sum-GFG)
+
+	Company: Amazon, Facebook, Google, Visa
+	Platform: Leetcode- 560, GFG, Coding Ninjas
+	Striver’s DSA sheet
+
+	Description
+	Given an array of integers nums and an integer k, return the total number of subarrays
+	whose sum equals to k.
+	A subarray is a contiguous non-empty sequence of elements within an array.
+
+	Example 1:
+	Input: nums = [1,1,1], k = 2
+	Output: 2
+
+	Example 2:
+	Input: nums = [1,2,3], k = 3
+	Output: 2
+
+	Constraints:
+	1 <= nums.length <= 2 * 104
+	-1000 <= nums[i] <= 1000
+	-107 <= k <= 107
+
+*/
+
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int sum = 0;
+        int count = 0;
+
+        for(int data : nums) {
+
+            sum = sum + data;
+
+            if(sum == k)
+                count++;
+
+            int target = sum - k;
+
+            if(hm.containsKey(target)) {
+
+                count = count + hm.get(target);
+            }
+            hm.put(sum, hm.getOrDefault(sum, 0)+1);
+        }
+        return count;
+    }
+}
