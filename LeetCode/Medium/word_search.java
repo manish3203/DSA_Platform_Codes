0cf450cd8129f42046c3fb90b1247e2df0c7de79
manish3203 <manishchavan3203@gmail.com>
2024-02-08 19:58:31 +0530

    79_Word Search

---
+++ LeetCode/Medium/word_search.java
@@ -0,0 +1,81 @@
+/*
+
+	Word Search
+
+	Company: Google, Adobe, Wipro, Ola, uber+23
+	Platform : Leetcode- 79
+	Fraz’s DSA sheet
+
+	Description :
+	Given an m x n grid of characters board and a string word, return true if word
+	exists in the grid.
+
+	The word can be constructed from letters of sequentially adjacent cells, where
+	adjacent cells are horizontally or vertically neighboring. The same letter cell may not be
+	used more than once.
+
+	Example 1:
+	Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+	Output: true
+	
+	Example 2:
+	Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+	Output: true
+
+	Example 3:
+	Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+	Output: false
+
+	Constraints:
+	m == board.length
+	n = board[i].length
+	1 <= m, n <= 6
+	1 <= word.length <= 15
+	board and word consists of only lowercase and uppercase English letters.
+
+
+*/
+
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean result = false;
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(dfs(board,word,i,j,0))
+                {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+    public boolean dfs(char[][] board, String word, int i, int j, int k)
+    {
+        int m = board.length;
+        int n = board[0].length;
+        if(i<0 || j<0 || i>=m || j>=n)
+        {
+            return false;
+        }
+        if(board[i][j] == word.charAt(k))
+        {
+            char temp = board[i][j];
+            board[i][j]='#';
+            if(k==word.length()-1)
+            {
+                return true;
+            }
+            else if(dfs(board, word, i-1, j, k+1) ||dfs(board, word, i+1, j, k+1) ||dfs(board, word, i, j-1, k+1) ||dfs(board, word, i, j+1, k+1))
+            {
+                return true;
+            }
+            board[i][j]=temp;
+        }
+        return false;
+    }
+}
